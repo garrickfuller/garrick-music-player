c268bb7a9fd4c62e5b7db81a9d9a8cfa5dd56442
garrickfuller <garrick.fuller108@gmail.com>
2024-05-21 23:01:01 -0400

    initialize spring boot backend

---
+++ music-player-backend/src/main/java/com/garrickfuller/musicplayerbackend/MusicPlayerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.garrickfuller.musicplayerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicPlayerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicPlayerBackendApplication.class, args);
+	}
+
+}
---
+++ music-player-backend/src/test/java/com/garrickfuller/musicplayerbackend/MusicPlayerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.garrickfuller.musicplayerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicPlayerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
